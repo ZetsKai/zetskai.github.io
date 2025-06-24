6cc9aef6f52892db51b589651498d04578b02376
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-06-24 07:34:22 -0400

    Update router.js

--- router.js
+++ router.js
@@ -1,19 +1,20 @@
-export function router(event) {
-    if (event === null) return;
+export class Router {
+    static getRoute(event) {
+        if (event === null) return;
 
-    const currentPageCount = window.history.state.count;
-    const btn = event.currentTarget;
-    const route = btn.href;
+        const btnId = event.detail.btnId;
+        const route = event.detail.route;
 
-    window.history.pushState({page: route, btn: btn.id, count: (currentPageCount - 1)}, null, '');
-    goToPage(route);
-}
+        window.history.pushState({page: route, btn: btnId }, null, '');
+        Router.goToPage(route);
+    }
 
-export function goToPage(route) {
-    const rootContainer = document.querySelector('#root');
+    static goToPage(route) {
+        const rootContainer = document.querySelector('#root');
 
-    fetch(route)
-    .then(data => data.text())
-    .then(data => rootContainer.innerHTML = data)
-    .catch(error => console.log(error));
+        fetch(route)
+        .then(data => data.text())
+        .then(data => rootContainer.innerHTML = data)
+        .catch(error => console.log(error));
+    }
 }
