b45b8711c6e85195daa0086e561476472a0587bb
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-06-24 07:46:20 -0400

    Update search.js

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,9 +1,9 @@
-import { defineCustomElement } from "../../utils/defineCustomElement.js";
 import { PostsTabs } from "./components/posts-tabs/posts-tabs.js";
 
+// TODO - Contain this bitchass.
 function switchSubpage(subpageSwitchEvent) {
+	const searchPage = document.querySelector('.search.page');
 	const subpageName = subpageSwitchEvent.detail;
-	const searchPage = document.querySelector('.search');
 	const subpage = searchPage.querySelector(`.${subpageName}`);
 	const selectedSubpage = searchPage.querySelector('.search__subpage--selected');
 	
@@ -11,34 +11,4 @@ function switchSubpage(subpageSwitchEvent) {
 	subpage.classList.add('search__subpage--selected');
 }
 
-document.addEventListener('postSwitch', switchSubpage);
-defineCustomElement('posts-tabs', PostsTabs);
-
-
-// export class SearchPage extends HTMLElement {
-// 	constructor() {
-// 		super();
-
-// 		this.attachShadow({ mode: 'open' });
-// 	}
-
-// 	connectedCallback() {
-// 		const template = document.querySelector('#search-page').content;
-// 		this.shadowRoot.appendChild(template);
-
-// 		defineCustomElement('posts-tabs', PostsTabs);
-// 		this.shadowRoot.addEventListener('postSwitch', this.switchSubpage.bind(this));
-// 	}
-
-// 	disconnectedCallback() {
-
-// 	}
-
-// 	switchSubpage(subpageName) {
-// 		const subpage = this.shadowRoot.querySelector(`.${subpageName}`);
-// 		const selectedSubpage = this.shadowRoot.querySelector('.subpage--selected');
-
-// 		selectedSubpage.classList.remove('subpage--selected');
-// 		subpage.classList.add('subpage--selected');
-// 	}
-// }
+document.addEventListener('switch-subpage', switchSubpage);
