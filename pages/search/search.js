385caaa7bb4aa52567a7793145f4750d5195970f
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-06-26 11:05:37 -0400

    Update search.js

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,10 +1,14 @@
-import { PostsTabs } from "./components/posts-tabs/posts-tabs.js";
+import { PostsTabs } from "./components/posts-tabs.js";
+import { Post } from './components/post.js';
+import { Gallery } from "./components/gallery.js";
 
 // TODO - Contain this bitchass.
 function switchSubpage(subpageSwitchEvent) {
 	const searchPage = document.querySelector('.search.page');
 	const subpageName = subpageSwitchEvent.detail;
-	const subpage = searchPage.querySelector(`.${subpageName}`);
+	const subpage = searchPage.querySelector(`.${subpageName}:not(.search__subpage--selected)`);
+	console.log(subpage);
+	
 	const selectedSubpage = searchPage.querySelector('.search__subpage--selected');
 	
 	selectedSubpage.classList.remove('search__subpage--selected');
