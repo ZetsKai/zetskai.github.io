524aa4a070d89c44a150a8b459dac1c979e85fe6
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-06-26 11:05:05 -0400

    Create hostResets.js

---
+++ assets/style/hostResets.js
@@ -0,0 +1,12 @@
+export const hostResets = /*css*/`
+    *:not(:host), *::before, *::after {
+        box-sizing: border-box;
+    }
+
+    html, body, div, main, article, section, aside, footer, nav,
+    h1, h2, h3, h4, h5, h6, p, a, span
+    img, video {
+        padding: 0;
+        margin: 0;
+    }
+`;
