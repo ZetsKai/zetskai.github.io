0b80969e2738f0c042261a1265d151f478444cc3
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-10 09:05:01 -0400

    Update search.js

--- pages/search/search.js
+++ pages/search/search.js
@@ -4,15 +4,40 @@ import { Gallery } from "./components/gallery.js";
 
 // TODO - Contain this bitchass.
 function switchSubpage(subpageSwitchEvent) {
-	const searchPage = document.querySelector('.search.page');
 	const subpageName = subpageSwitchEvent.detail;
-	const subpage = searchPage.querySelector(`.${subpageName}:not(.search__subpage--selected)`);
-	console.log(subpage);
+	const subpage = document.querySelector(`.${subpageName}:not(.search__subpage--selected)`);
 	
-	const selectedSubpage = searchPage.querySelector('.search__subpage--selected');
+	const selectedSubpage = document.querySelector('.search__subpage--selected');
 	
 	selectedSubpage.classList.remove('search__subpage--selected');
 	subpage.classList.add('search__subpage--selected');
 }
+
+setTimeout(function() {
+	const imageViewContainer = document.querySelector('.image-view__container');
+	const submenu = document.querySelector('.image-view__submenu');
+	let amount = 0;
+
+	document.querySelector('.image-view__exit').addEventListener('click', () => document.querySelector('.image-view').style.display = 'none');
+
+	console.log(submenu.getBoundingClientRect().height);
+
+	imageViewContainer.addEventListener('touchmove', (e) => {
+		const tempAmount = e.touches[0].screenY;
+		const currentSubmenuHeight = submenu.getBoundingClientRect().height;
+
+		if (amount >= tempAmount) {
+			console.log('up');
+			submenu.style.height = `${currentSubmenuHeight + 12}px`
+		};
+		if (amount <= tempAmount) {
+			console.log('down');
+			submenu.style.height = `${currentSubmenuHeight - 12}px`
+		}
+		amount = tempAmount;
+	});
+
+	const downloadButton = document.querySelector('.quick-actions__btn--download');
+}, 2000);
 
 document.addEventListener('switch-subpage', switchSubpage);
