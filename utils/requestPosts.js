86326fbb7c4fbe6dacab0c6e2ed962b97cfd29c7
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-17 07:21:18 -0400

    Update requestPosts.js

--- utils/requestPosts.js
+++ utils/requestPosts.js
@@ -3,8 +3,10 @@ import { getUserInfo } from "./getUserInfo.js";
 export async function requestPosts() {
     // const reqUrl = `https://e621.net/posts.json?login=${username}&api_key=${apiKey}`;
     const user = getUserInfo();
+    // const nsfw = 'e621';
+    const sfw = 'e926';
 
-    const response = await fetch('https://e621.net/posts.json?limit=8', {
+    const response = await fetch(`https://${sfw}.net/posts.json?limit=8`, {
         headers: {
             'Authorization': 'Basic ' + btoa(`${user.username}:${user.apiKey}`),
             'User-Agent': 'Foxhole/1.0 (ZetsKai)'
