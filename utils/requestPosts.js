a563922061574e66bc38695dcb4c488a014ef781
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-17 07:05:24 -0400

    Create requestPosts.js

---
+++ utils/requestPosts.js
@@ -0,0 +1,30 @@
+import { getUserInfo } from "./getUserInfo.js";
+
+export async function requestPosts() {
+    // const reqUrl = `https://e621.net/posts.json?login=${username}&api_key=${apiKey}`;
+    const user = getUserInfo();
+
+    const response = await fetch('https://e621.net/posts.json?limit=8', {
+        headers: {
+            'Authorization': 'Basic ' + btoa(`${user.username}:${user.apiKey}`),
+            'User-Agent': 'Foxhole/1.0 (ZetsKai)'
+        }
+    })
+    .then(res => res.json());
+
+    return response;
+}
+
+export async function requestSpecificPost(postId) {
+    const user = getUserInfo();
+
+    const response = await fetch(`https://e621.net/posts/${postId}.json`, {
+        headers: {
+            'Authorization': 'Basic ' + btoa(`${user.username}:${user.apiKey}`),
+            'User-Agent': 'Foxhole/1.0 (ZetsKai)'
+        }
+    })
+    .then(res => res.json());
+
+    return response;
+}
