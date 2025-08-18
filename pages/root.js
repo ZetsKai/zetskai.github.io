61c201375b2d63cde4c098c56d7a9f10af374270
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-18 08:21:19 -0400

    Update root.js

--- pages/root.js
+++ pages/root.js
@@ -1,7 +1,3 @@
-import './search/search.js';
-import { Appearance } from './settings/subpages/appearance/appearance.js';
-
-
 export async function appendToRoot(html) {
     const rootContainer = document.querySelector('#root');
     const container = document.createElement('div');
