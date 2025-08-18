50b909b2216f5b0d9cbd667018c7a9fd85e67ca3
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-18 08:51:57 -0400

    Update gallery.js

--- pages/search/components/gallery.js
+++ pages/search/components/gallery.js
@@ -58,44 +58,45 @@ export class Gallery extends HTMLElement {
         const columns = this.shadowRoot.querySelector('slot').assignedElements();
         if (columns.length === 0) return;
 
-        // const posts = (await requestPosts()).posts;
-        const posto = [
-            'https://static1.e926.net/data/6e/13/6e136ee7dbe6c1c15740ff4be5496c33.jpg',
-            'https://static1.e926.net/data/8d/eb/8deb2ae9fe81f9f6383a24c591bdd3be.png',
-            'https://static1.e926.net/data/22/f6/22f6672e35a8aab9f4dc0140f46f9b96.jpg',
-            'https://static1.e926.net/data/8b/a7/8ba7610b0a2b91d91aebf4a55e131aa7.png',
-            'https://static1.e926.net/data/ab/b7/abb730ed9734e6ec1c08c6d90be59be4.jpg',
-            'https://static1.e926.net/data/e2/d3/e2d3892a36c9bc4e2e8e44720b07ee48.png',
-            'https://static1.e926.net/data/c0/fa/c0fa5293f1d1440c2d3f2c3e027d3c36.jpg',
-            'https://static1.e926.net/data/10/a5/10a562c5d6a6398716a7bcc9ada5f612.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
-            'https://static1.e926.net/data/0f/d0/0fd077c78f42bfd26db3a9cc5397edce.png'
-        ];
+        const posts = (await requestPosts()).posts;
+        // const posto = [
+        //     'https://static1.e926.net/data/6e/13/6e136ee7dbe6c1c15740ff4be5496c33.jpg',
+        //     'https://static1.e926.net/data/8d/eb/8deb2ae9fe81f9f6383a24c591bdd3be.png',
+        //     'https://static1.e926.net/data/22/f6/22f6672e35a8aab9f4dc0140f46f9b96.jpg',
+        //     'https://static1.e926.net/data/8b/a7/8ba7610b0a2b91d91aebf4a55e131aa7.png',
+        //     'https://static1.e926.net/data/ab/b7/abb730ed9734e6ec1c08c6d90be59be4.jpg',
+        //     'https://static1.e926.net/data/e2/d3/e2d3892a36c9bc4e2e8e44720b07ee48.png',
+        //     'https://static1.e926.net/data/c0/fa/c0fa5293f1d1440c2d3f2c3e027d3c36.jpg',
+        //     'https://static1.e926.net/data/10/a5/10a562c5d6a6398716a7bcc9ada5f612.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/4a/de/4adeba19a4b8cd5608024f059ac12f88.png',
+        //     'https://static1.e926.net/data/0f/d0/0fd077c78f42bfd26db3a9cc5397edce.png'
+        // ];
         
         let flag = 0;
-        posto.forEach(post => {
+        posts.forEach(post => {
             const postComp = document.createElement('post-image');
-            postComp.assignImage(post);
+            // postComp.assignImage(post);
+            postComp.storePostData(post);
             // postComp.assignImage(post.preview.url);
             columns[flag].appendChild(postComp);
 
