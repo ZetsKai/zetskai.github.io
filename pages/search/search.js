334fff036eb7038b09e670d7a5652f84eb24a502
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-18 08:08:48 -0400

    Update search.js

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,6 +1,8 @@
 import { PostsTabs } from "./components/posts-tabs.js";
 import { Post } from './components/post.js';
 import { Gallery } from "./components/gallery.js";
+import { FullView } from "./subpages/full-view/full-view.js";
+import { Router } from "../../utils/router.js";
 
 // TODO - Contain this bitchass.
 function switchSubpage(subpageSwitchEvent) {
@@ -12,55 +14,20 @@ function switchSubpage(subpageSwitchEvent) {
 	subpage.classList.add('search__subpage--selected');
 }
 
-setTimeout(function() {
-	const imageViewContainer = document.querySelector('.image-view__container');
-	const submenu = document.querySelector('.image-view__submenu');
-	let oldFingerPosY = 0;
-	let newHeight = 0;
-
-	async function changeHeight() {
-		submenu.style.height = newHeight;
-	}
-
-	document.querySelector('.image-view__exit').addEventListener('click', () => document.querySelector('.image-view').style.display = 'none');
-
-	imageViewContainer.addEventListener('touchmove', (e) => {
-		const currentFingerPosY = e.touches[0].screenY;
-		const currentSubmenuHeight = submenu.getBoundingClientRect().height;
-		const calculation = oldFingerPosY - (currentFingerPosY);
-
-		newHeight = `${currentSubmenuHeight + calculation}px`;
-		oldFingerPosY = currentFingerPosY;
-		requestAnimationFrame(changeHeight);
-	}, { passive: true });
-
-	imageViewContainer.addEventListener('touchend', () => {
-		const currentSubmenuHeight = submenu.getBoundingClientRect().height;
-		const heightPercent = window.innerHeight * 0.25;
-		
-		if (currentSubmenuHeight > heightPercent) submenu.style.height = '44%';
-		else if (currentSubmenuHeight <= heightPercent) submenu.style.height = '0%';
+function fullView() {
+	Router.getRoute({
+		detail: {
+			btnId: 'search',
+			route: '/pages/search/subpages/full-view/full-view.html'
+		}
 	});
 
-	async function downloadImage() {
-		const src = 'https://static1.e926.net/data/6e/13/6e136ee7dbe6c1c15740ff4be5496c33.jpg';
- 		const image = await fetch(src);
- 		const imageBlog = await image.blob();
- 		const imageURL = URL.createObjectURL(imageBlog);
+	const fullViewEvent = new Event('full-view', {
+        bubbles: true,
+    })
 
-		const link = document.createElement('a')
- 		link.href = imageURL
- 		link.download = 'foxcrow-test.jpg'
- 		document.body.appendChild(link)
- 		link.click()
- 		document.body.removeChild(link)
-	}
+    // this.dispatchEvent(fullViewEvent);
+}
 
-	const downloadButton = document.querySelector('.quick-actions__btn--download');
-	downloadButton.addEventListener('click', (e) => {
-		console.log('downloading?');
-		downloadImage();
-	});
-}, 2000);
-
-document.addEventListener('switch-subpage', switchSubpage);
+document.addEventListener('switch-subpage', switchSubpage)
+document.addEventListener('full-view', fullView);
