6d285efcbe119cf8e58bdffc89b2f209edbd4630
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-18 08:31:21 -0400

    Create settings.js

---
+++ pages/settings/settings.js
@@ -0,0 +1,3 @@
+import { AccentButtons } from "./components/accentButtons.js";
+import { OptionButton } from "./components/optionButton.js";
+import { ThemeSwitch } from "./components/themeSwitch.js";
