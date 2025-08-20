cce876ac59991f6d94494b8373d5daf96668aa21
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-20 06:26:36 -0400

    Create FullView.js

---
+++ pages/search/subpages/FullView/FullView.js
@@ -0,0 +1,184 @@
+import { hostResets } from '../../../../assets/style/hostResets.js';
+import { defineCustomElement } from '../../../../utils/defineCustomElement.js';
+import './components/ScoreFav.js'
+import './components/Submenu.js';
+
+const style = /*css*/`
+    ${hostResets}
+
+    :host {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+        background-color: var(--inset-surface);
+    }
+
+    .header {
+        display: flex;
+        overflow: hidden;
+        justify-content: space-between;
+        align-items: center;
+        min-height: 0;
+        flex-shrink: 0;
+        padding: var(--spacing-xl);
+        background-color: var(--surface);
+        border-bottom: 1px solid var(--border);
+        color: var(--icon);
+    }
+
+    .header__exit, .header__info-btn {
+        background-color: transparent;
+        border: unset;
+        color: inherit;
+    }
+
+    .header__id {
+        min-width: 0;
+        flex-shrink: 1;
+    }
+    
+    .image-container {
+        display: flex;
+        position: relative;
+        justify-content: center;
+        align-items: center;
+        min-height: 0;
+        flex-basis: 100%;
+        flex-grow: 1;
+        padding: var(--spacing-xl);
+    }
+
+    .image-container__image {
+        max-width: 100%;
+        max-height: 100%;
+        border-radius: 8px;
+        border: 1px solid var(--border);
+    }
+
+    :host(.full-view--fullscreen) {
+        background-color: black;
+
+        .header {
+            flex-basis: 0;
+            padding: 0;
+        }
+
+        .image-container {
+            padding: 0;
+        }
+
+        .image-container__image {
+            border-radius: unset;
+            border: unset;
+        }
+
+        .score-fav {
+            display: none;
+        }
+
+        .submenu {
+            padding: 0;
+        }
+    }
+`;
+
+const template = document.createElement('template');
+template.innerHTML = /*html*/`
+    <div class="header">
+        <button class="header__exit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+                <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+                <path d="m9.88 12.71-4.95 4.95 1.41 1.41 2.83-2.83L12 13.41l2.12 2.13 3.54 3.53 1.41-1.41-4.95-4.95-.71-.71 5.66-5.66-1.41-1.41L12 10.59 6.34 4.93 4.93 6.34 10.59 12z"></path>
+            </svg>
+        </button>
+        <div class="header__id">
+            <span class="full-view__id-logo" style="font-weight: bolder;">ID</span>
+            <span class="full-view__id-num">99999</span>
+        </div>
+        <button class="header__info-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+                <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+                <path d="M12 22c5.51 0 10-4.49 10-10S17.51 2 12 2 2 6.49 2 12s4.49 10 10 10M11 7h2v2h-2zm0 4h2v6h-2z"></path>
+            </svg>
+        </button>
+    </div>
+    <div class="image-container">
+        <img class="image-container__image" src="https://static1.e926.net/data/6e/13/6e136ee7dbe6c1c15740ff4be5496c33.jpg" alt="" />
+        <score-fav></score-fav>
+    </div>
+    <sub-menu></sub-menu>
+
+    <style>${style}</style>
+`;
+
+export class FullView extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.append(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        const container = this.shadowRoot.querySelector('.image-container');
+	    const submenu = this.shadowRoot.querySelector('sub-menu');
+	    let oldFingerPosY;
+
+        const cancelSelect = e => {
+            e.preventDefault();
+            return false;
+        }
+
+        container.addEventListener('click', this.fullscreen.bind(this));
+
+        container.addEventListener('touchstart', (e) => {
+            document.addEventListener('selectstart', cancelSelect)
+            oldFingerPosY = e.touches[0].screenY;
+            submenu.classList.add('submenu--open');
+        });
+
+	    container.addEventListener('touchmove', (e) => {
+	    	const currentFingerPosY = e.touches[0].screenY;
+	    	const fingerPosCalculation = oldFingerPosY - (currentFingerPosY);
+
+	    	oldFingerPosY = currentFingerPosY;
+            submenu.setHeight(fingerPosCalculation);
+	    }, { passive: true });
+
+	    container.addEventListener('touchend', () => {
+            submenu.setHeight();
+            document.removeEventListener('selectstart', cancelSelect);
+	    }, { passive: true });
+
+	    this.shadowRoot.querySelector('.header__exit').addEventListener('click', () => history.back());
+    }
+
+    disconnectedCallback() {
+        console.log('bye <full-view!/>');
+    }
+
+    fullscreen() {
+        this.classList.toggle('full-view--fullscreen');
+        const submenu = this.shadowRoot.querySelector('.submenu');
+        submenu.classList.remove('submenu--open');
+        submenu.style.height = '0%';
+
+        const shittySafariForceRepaint = () => {
+            this.style.display = 'none';
+            this.offsetHeight;
+            this.style.display = 'flex';
+        }
+
+        // const chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36';
+        // const arc    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36';
+        // const edge   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0';
+        // const opera  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0s';
+        // const safari = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1';
+
+        const isSafari = /iPhone/i.test(navigator.userAgent);
+        if (isSafari)
+            requestAnimationFrame(shittySafariForceRepaint);
+    }
+}
+defineCustomElement('full-view', FullView);
