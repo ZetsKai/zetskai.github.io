0ee6f16bbadcf14ca78272456b84505001834e19
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-20 07:50:02 -0400

    Update ScoreFav.js

--- pages/search/subpages/FullView/components/ScoreFav.js
+++ pages/search/subpages/FullView/components/ScoreFav.js
@@ -1,5 +1,6 @@
 import { defineCustomElement } from "../../../../../utils/defineCustomElement.js";
 import { hostResets } from "../../../../../assets/style/hostResets.js";
+import { store } from "../../../../../utils/store.js";
 const style = /*css*/`
     ${hostResets}
 
@@ -49,7 +50,7 @@ template.innerHTML = /*html*/`
                 <path d="M12.87 3.51c-.35-.63-1.39-.63-1.74 0l-9 16c-.17.31-.17.69 0 1s.51.5.86.5h18c.36 0 .68-.19.86-.5s.18-.69 0-1z"></path>
             </svg>
         </button>
-        <div class="score__counter">99999+</div>
+        <div class="score__counter">0!</div>
         <button class="score__button">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
                 <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
@@ -75,7 +76,9 @@ export class ScoreFav extends HTMLElement {
         this.shadowRoot.append(template.content.cloneNode(true));
     }
 
-    connectedCallback() {}
+    connectedCallback() {
+        this.shadowRoot.querySelector('.score__counter').innerHTML = store.selectedPost.score.total
+    }
 
     disconnectedCallback() {}
 }
