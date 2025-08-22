c88af294cfa6b89390301f3759dfdcb5ec6936c1
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-22 09:15:18 -0400

    Update Submenu.js

--- pages/search/subpages/FullView/components/Submenu.js
+++ pages/search/subpages/FullView/components/Submenu.js
@@ -48,7 +48,7 @@ const template = document.createElement('template');
 template.innerHTML = /*html*/`
     <div class="link-dump"></div>
     <nav class="quick-actions">
-        <button class="quick-actions__button">
+        <button class="quick-actions__button quick-actions__button--url">
             <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
                 <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
                 <path d="M9.88 18.36a3 3 0 0 1-4.24 0 3 3 0 0 1 0-4.24l2.83-2.83-1.41-1.41-2.83 2.83a5.003 5.003 0 0 0 0 7.07c.98.97 2.25 1.46 3.54 1.46s2.56-.49 3.54-1.46l2.83-2.83-1.41-1.41-2.83 2.83ZM12.71 4.22 9.88 7.05l1.41 1.41 2.83-2.83a3 3 0 0 1 4.24 0 3 3 0 0 1 0 4.24l-2.83 2.83 1.41 1.41 2.83-2.83a5.003 5.003 0 0 0 0-7.07 5.003 5.003 0 0 0-7.07 0Z"></path><path d="m16.95 8.46-.71-.7-.7-.71-4.25 4.24-4.24 4.25.71.7.7.71 4.25-4.24z"></path>
@@ -62,7 +62,7 @@ template.innerHTML = /*html*/`
             </svg>
             <p class="quick-actions__button-title">Save</p>
         </button>
-        <button class="quick-actions__button">
+        <button class="quick-actions__button quick-actions__button--flag">
             <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
                 <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
                 <path d="M5 15.64c2-.87 4.28-.76 6.18.33 1.37.78 2.89 1.18 4.42 1.18 1.12 0 2.24-.21 3.32-.64l1.45-.58A1 1 0 0 0 21 15V4c0-.33-.17-.64-.44-.83a1 1 0 0 0-.93-.1l-1.45.58c-1.97.79-4.16.63-6-.42A8.9 8.9 0 0 0 3.77 3l-.21.1a1 1 0 0 0-.55.89v18h2v-6.36Z"></path>
@@ -77,6 +77,9 @@ template.innerHTML = /*html*/`
 `;
 
 export class Submenu extends HTMLElement {
+    #urlButton;
+    #downloadButton;
+    #flagButton;
     constructor() {
         super();
 
@@ -85,13 +88,16 @@ export class Submenu extends HTMLElement {
     }
 
     connectedCallback() {
-        const downloadButton = this.shadowRoot.querySelector('.quick-actions__button--download');
-        downloadButton.addEventListener('click', this.downloadImage.bind(this));
+        this.#urlButton = this.shadowRoot.querySelector('.quick-actions__button--url');
+        this.#downloadButton = this.shadowRoot.querySelector('.quick-actions__button--download');
+
+        this.#downloadButton.addEventListener('click', this.#downloadImage.bind(this));
+        this.#urlButton.addEventListener('click', this.#copyToClipboard.bind(this));
     }
 
     disconnectedCallback() {
-        const downloadButton = this.shadowRoot.querySelector('.quick-actions__button--download');
-        downloadButton.removeEventListener('click', this.downloadImage.bind(this));
+        this.#downloadButton.removeEventListener('click', this.#downloadImage.bind(this));
+        this.#urlButton.removeEventListener('click', this.#copyToClipboard.bind(this));
     }
 
     setHeight(fingerPosCalculation) {
@@ -115,8 +121,12 @@ export class Submenu extends HTMLElement {
 
         requestAnimationFrame(() => this.style.height = `${newHeight}`);
     }
+    tempReset() {
+        this.classList.remove('submenu--open');
+        this.style.height = '0%';
+    }
 
-    async downloadImage() {
+    async #downloadImage() {
         const linkDump = this.shadowRoot.querySelector('.link-dump');
 		const src = store.selectedPost.file.url;
  		const image = await fetch(src);
@@ -130,5 +140,11 @@ export class Submenu extends HTMLElement {
  		link.click()
  		linkDump.removeChild(link)
 	}
+
+    #copyToClipboard() {
+        const urlToCopy = store.selectedPost.file.url;
+        console.log(urlToCopy);
+        navigator.clipboard.writeText(urlToCopy);
+    }
 }
 defineCustomElement('sub-menu', Submenu);
