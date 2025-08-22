87c08a2dd12284accd4025209448db89463fb19b
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-08-22 09:13:18 -0400

    Update FullView.js

--- pages/search/subpages/FullView/FullView.js
+++ pages/search/subpages/FullView/FullView.js
@@ -1,101 +1,85 @@
-import { hostResets } from '../../../../assets/style/hostResets.js';
-import { defineCustomElement } from '../../../../utils/defineCustomElement.js';
-import { store } from '../../../../utils/store.js';
-import './components/ImageContainer.js';
-import './components/Submenu.js';
-
+import { defineCustomElement } from "../../../../../utils/defineCustomElement.js";
+import { hostResets } from "../../../../../assets/style/hostResets.js";
+import { store } from "../../../../../utils/store.js";
 const style = /*css*/`
     ${hostResets}
 
     :host {
+        flex-shrink: 0;
+        min-height: 0;
+        max-height: 44%;
+        height: 0%;
+        // padding: var(--spacing-lg);
+        padding: 0;
+        border-radius: 8px 8px 0 0;
+        background-color: var(--surface);
+        border-top: 1px solid var(--border);
+        overflow: hidden;
+        /* transition: height 1s linear; */
+    }
+    :host(.submenu--open) {
+        padding: var(--spacing-lg);
+    }
+
+    .link-dump { display: none !important; }
+
+    .quick-actions {
+        display: flex;
+        gap: var(--spacing-lg);
+        padding-bottom: var(--spacing-lg);
+        border-bottom: 1px solid var(--border);
+    }
+
+    .quick-actions__button {
         display: flex;
         flex-direction: column;
-        width: 100%;
-        height: 100%;
-        background-color: var(--inset-surface);
-    }
-
-    .header {
-        display: flex;
-        overflow: hidden;
-        justify-content: space-between;
         align-items: center;
-        min-height: 0;
-        flex-shrink: 0;
-        padding: var(--spacing-xl);
-        background-color: var(--surface);
-        border-bottom: 1px solid var(--border);
+        gap: var(--spacing-md);
+        flex-basis: 100%;
+        padding: var(--spacing-md);
+        border-radius: 8px;
+        background-color: var(--inset-surface);
+        border: 1px solid var(--border);
         color: var(--icon);
-    }
-
-    .header__exit, .header__info-btn {
-        background-color: transparent;
-        border: unset;
-        color: inherit;
-    }
-
-    .header__id {
-        min-width: 0;
-        flex-shrink: 1;
-    }
-
-    :host(.full-view--fullscreen) {
-        background-color: black;
-
-        .header {
-            flex-basis: 0;
-            padding: 0;
-        }
-
-        .image-container {
-            padding: 0;
-        }
-
-        .image-container__image {
-            border-radius: unset;
-            border: unset;
-        }
-
-        .score-fav {
-            display: none;
-        }
-
-        .submenu {
-            padding: 0;
-        }
     }
 `;
 
 const template = document.createElement('template');
 template.innerHTML = /*html*/`
-    <div class="header">
-        <button class="header__exit">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+    <div class="link-dump"></div>
+    <nav class="quick-actions">
+        <button class="quick-actions__button quick-actions__button--url">
+            <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
                 <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
-                <path d="m9.88 12.71-4.95 4.95 1.41 1.41 2.83-2.83L12 13.41l2.12 2.13 3.54 3.53 1.41-1.41-4.95-4.95-.71-.71 5.66-5.66-1.41-1.41L12 10.59 6.34 4.93 4.93 6.34 10.59 12z"></path>
+                <path d="M9.88 18.36a3 3 0 0 1-4.24 0 3 3 0 0 1 0-4.24l2.83-2.83-1.41-1.41-2.83 2.83a5.003 5.003 0 0 0 0 7.07c.98.97 2.25 1.46 3.54 1.46s2.56-.49 3.54-1.46l2.83-2.83-1.41-1.41-2.83 2.83ZM12.71 4.22 9.88 7.05l1.41 1.41 2.83-2.83a3 3 0 0 1 4.24 0 3 3 0 0 1 0 4.24l-2.83 2.83 1.41 1.41 2.83-2.83a5.003 5.003 0 0 0 0-7.07 5.003 5.003 0 0 0-7.07 0Z"></path><path d="m16.95 8.46-.71-.7-.7-.71-4.25 4.24-4.24 4.25.71.7.7.71 4.25-4.24z"></path>
             </svg>
+            <p class="quick-actions__button-title">URL</p>
         </button>
-        <div class="header__id">
-            <span class="header__id-logo" style="font-weight: bolder;">ID</span>
-            <span class="header__id-num">99999</span>
-        </div>
-        <button class="header__info-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+        <button class="quick-actions__button quick-actions__button--download">
+            <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
                 <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
-                <path d="M12 22c5.51 0 10-4.49 10-10S17.51 2 12 2 2 6.49 2 12s4.49 10 10 10M11 7h2v2h-2zm0 4h2v6h-2z"></path>
+                <path d="M5 21h14c1.1 0 2-.9 2-2V8c0-.27-.11-.52-.29-.71l-4-4A1 1 0 0 0 16 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2M7 5h4v2h2V5h2v4H7zm0 8c0-.55.45-1 1-1h8c.55 0 1 .45 1 1v6H7z"></path>
             </svg>
+            <p class="quick-actions__button-title">Save</p>
         </button>
-    </div>
-    <image-container class="image-container"></image-container>
-    <sub-menu></sub-menu>
+        <button class="quick-actions__button quick-actions__button--flag">
+            <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+                <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+                <path d="M5 15.64c2-.87 4.28-.76 6.18.33 1.37.78 2.89 1.18 4.42 1.18 1.12 0 2.24-.21 3.32-.64l1.45-.58A1 1 0 0 0 21 15V4c0-.33-.17-.64-.44-.83a1 1 0 0 0-.93-.1l-1.45.58c-1.97.79-4.16.63-6-.42A8.9 8.9 0 0 0 3.77 3l-.21.1a1 1 0 0 0-.55.89v18h2v-6.36Z"></path>
+            </svg>
+            <p class="quick-actions__button-title">Report</p>
+        </button>
+    </nav>
+    <div class="expander">Description</div>
+    <div class="expander">Comments</div>
 
     <style>${style}</style>
 `;
 
-export class FullView extends HTMLElement {
-    #submenu;
-    #postData
-
+export class Submenu extends HTMLElement {
+    #urlButton;
+    #downloadButton;
+    #flagButton;
     constructor() {
         super();
 
@@ -104,53 +88,63 @@ export class FullView extends HTMLElement {
     }
 
     connectedCallback() {
-	    this.#submenu = this.shadowRoot.querySelector('sub-menu');
-        
-        this.#postData = store.selectedPost
-        this.shadowRoot.querySelector('.header__id-num').innerHTML = this.#postData.id;
-        
-        this.addEventListener('fullscreen', this.#fullscreen.bind(this));
-        this.addEventListener('submenuMove', this.#handleSubmemuHeight);
-        this.addEventListener('submenuDrop', this.#handleSubmenuDrop);
+        this.#urlButton = this.shadowRoot.querySelector('.quick-actions__button--url');
+        this.#downloadButton = this.shadowRoot.querySelector('.quick-actions__button--download');
 
-	    this.shadowRoot.querySelector('.header__exit').addEventListener('click', this.#closeFullView);
+        this.#downloadButton.addEventListener('click', this.#downloadImage.bind(this));
+        this.#urlButton.addEventListener('click', this.#copyToClipboard.bind(this));
     }
 
     disconnectedCallback() {
-        this.removeEventListener('fullscreen', this.#fullscreen.bind(this));
-        this.removeEventListener('submenuMove', this.#handleSubmemuHeight);
-        this.removeEventListener('submenuDrop', this.#handleSubmenuDrop);
-
-	    this.shadowRoot.querySelector('.header__exit').removeEventListener('click', this.#closeFullView);
+        this.#downloadButton.removeEventListener('click', this.#downloadImage.bind(this));
+        this.#urlButton.removeEventListener('click', this.#copyToClipboard.bind(this));
     }
 
-    #fullscreen() {
-        this.classList.toggle('full-view--fullscreen');
-        this.#submenu.classList.remove('submenu--open');
-        this.#submenu.style.height = '0%';
+    setHeight(fingerPosCalculation) {
+        this.classList.add('submenu--open')
+        const currentSubmenuHeight = this.getBoundingClientRect().height;
+        let newHeight;
 
-        // const chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36';
-        // const arc    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36';
-        // const edge   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0';
-        // const opera  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0s';
-        // const safari = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1';
+        if (fingerPosCalculation == undefined) {
+            const halfHeight = (window.innerHeight * 0.44) * 0.5;
 
-        const isSafari = /iPhone/i.test(navigator.userAgent);
-        if (isSafari)
-            requestAnimationFrame(this.#shittySafariForceRepaint);
+            if (currentSubmenuHeight > halfHeight) {
+                newHeight = '44%';
+                this.classList.add('submenu--open')
+            }
+		    else if (currentSubmenuHeight <= halfHeight) {
+                newHeight = '0%';
+                this.classList.remove('submenu--open');
+            }
+        }
+        else newHeight = `${currentSubmenuHeight + fingerPosCalculation}px`;
+
+        requestAnimationFrame(() => this.style.height = `${newHeight}`);
+    }
+    tempReset() {
+        this.classList.remove('submenu--open');
+        this.style.height = '0%';
     }
 
-    #handleSubmemuHeight(fingerPostCalculationInEventDetail) {
-		this.classList.remove('full-view--fullscreen');
-		this.#submenu.setHeight(fingerPostCalculationInEventDetail.detail);
+    async #downloadImage() {
+        const linkDump = this.shadowRoot.querySelector('.link-dump');
+		const src = store.selectedPost.file.url;
+ 		const image = await fetch(src);
+ 		const imageBlog = await image.blob();
+ 		const imageURL = URL.createObjectURL(imageBlog);
+
+		const link = document.createElement('a')
+ 		link.href = imageURL
+ 		link.download = `${store.selectedPost.id}.${store.selectedPost.file.ext}`
+ 		linkDump.appendChild(link)
+ 		link.click()
+ 		linkDump.removeChild(link)
 	}
-    #handleSubmenuDrop() { this.#submenu.setHeight(); };
-    #closeFullView() { history.back(); };
 
-	#shittySafariForceRepaint() {
-        this.style.display = 'none';
-        this.offsetHeight;
-        this.style.display = 'flex';
-    };
+    #copyToClipboard() {
+        const urlToCopy = store.selectedPost.file.url;
+        console.log(urlToCopy);
+        navigator.clipboard.writeText(urlToCopy);
+    }
 }
-defineCustomElement('full-view', FullView);
+defineCustomElement('sub-menu', Submenu);
