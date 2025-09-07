e6f33763455591676d8c11fa4b78003dc1e6fbeb
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-09-07 15:36:11 -0400

    Update components.js

--- components/components.js
+++ components/components.js
@@ -1,3 +1,3 @@
 import { ButtonGrid } from "./buttonGrid/buttonGrid.js";
-import { ModeSwitch } from "./modeSwitch.js";
+import { SegmentedControl } from "./SegmentedControl.js";
 import { Toggle } from "./toggle.js";
