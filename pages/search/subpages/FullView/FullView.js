daa916af61f5c812454f38e9722c7757f5d9a510
ZetsKai <88992184+ZetsKai@users.noreply.github.com>
2025-09-07 18:48:43 -0400

    Update FullView.js

--- pages/search/subpages/FullView/FullView.js
+++ pages/search/subpages/FullView/FullView.js
@@ -1,234 +1,214 @@
-import { defineCustomElement } from "../../../utils/defineCustomElement.js";
-import { hostResets } from "../../../assets/style/hostResets.js";
+import { hostResets } from '../../../../assets/style/hostResets.js';
+import { defineCustomElement } from '../../../../utils/defineCustomElement.js';
+import { store } from '../../../../utils/store.js';
+import './components/ImageContainer.js';
+import './components/ScoreFav.js';
+import './components/Submenu.js';
 
 const style = /*css*/`
     ${hostResets}
 
     :host {
-        z-index: 8;
-        position: absolute;
-        bottom: 0;
         display: none;
+        z-index; 99;
+        flex-direction: column;
         width: 100%;
-        height: 75%;
-        max-height: 100%;
-        background-color: var(--surface);
+        height: 100%;
+        background-color: var(--background);
     }
 
     :host([open]) {
-        display: block;
-    }
-
-    .shadow {
-        position: absolute;
-        bottom: 100%;
-        width: 100%;
-        height: 100vh;
-        background-color: rgba(0, 0, 0, 0.1);
-        backdrop-filter: blur(1px);
-        z-index: 255;
-    }
-
-    .scrollable-container {
         display: flex;
-        flex-direction: column;
-        gap: var(--spacing-lg);
-        padding: var(--spacing-lg);
-        height: 100%;
-        overflow-y: scroll;
     }
 
-    .section {
+    .link-dump { display: none !important; }
+
+    .header {
         display: flex;
-        flex-direction: column;
-        gap: var(--spacing-md);
-
-        /* ! - not working for some reason. */
-        &:not(:last-of-type) {
-            padding-bottom: var(--spacing-lg);
-            border-bottom: 1px solid var(--border);
-        }
+        overflow: hidden;
+        justify-content: space-between;
+        align-items: center;
+        min-height: 0;
+        flex-shrink: 0;
+        padding: var(--spacing-xl);
+        background-color: var(--surface);
+        border-bottom: 1px solid var(--border);
+        color: var(--icon);
     }
 
-    .section__title {
-        display: block;
+    .header__exit, .header__info-btn {
+        background-color: transparent;
+        border: unset;
+        color: inherit;
     }
 
-    .section__dropdown {
-        padding: var(--spacing-md);
-        border-radius: 100px;
-        font-size: 16px;
-        background-color: var(--inset-surface);
-        border: 1px solid var(--border);
-    }
-    
-    segmented-control[disabled] {
-        display: none;
+    .header__id {
+        min-width: 0;
+        flex-shrink: 1;
     }
 
-    .section__buttons {
-        display: flex;
-        flex-wrap: wrap;
-        gap: var(--spacing-md);
-    }
+    :host([fullscreen]) {
+        background-color: black;
 
-    .section__button {
-        appearance: none;
-        padding: var(--spacing-md);
-        border-radius: 100px;
-        background-color: var(--inset-surface);
-        border: 1px solid var(--border);
-
-        &::before {
-            display: block;
-            content: attr(text);
+        .header {
+            flex-basis: 0;
+            padding: 0;
         }
 
-        &:checked {
-            font-weight: bolder;
-            color: white;
-            background-color: var(--fill-accent);
-            border: none;
+        .image-container {
+            padding: 0;
         }
 
-        &:disabled {
-            color: gray;
+        .image-container__image {
+            border-radius: unset;
+            border: unset;
         }
-    }
 
-    .section__button--safe:checked {
-        background-color: var(--fill-safe);
-    }
-    .section__button--questionable:checked {
-        background-color: var(--fill-questionable);
-    }
-    .section__button--explicit:checked {
-        background-color: var(--fill-explicit);
-    }
+        .score-fav {
+            display: none;
+        }
 
-    .confirmation {
-        display: flex;
-        justify-content: flex-end;
-        gap: var(--spacing-md);
-        margin-top: auto;
-    }
+        .submenu {
+            padding: 0;
+        }
 
-    .confirmation__button {
-        flex-basis: 100%;
-        padding: var(--spacing-lg);
-        border-radius: 100px;
-        background-color: var(--inset-surface);
-        border: 1px solid var(--border);
-    }
-
-    .confirmation__button--apply {
-        font-weight: bolder;
-        color: white;
-        background-color: var(--fill-accent);
-        border: none;
     }
 `;
 
 const template = document.createElement('template');
 template.innerHTML = /*html*/`
-    <div class="shadow"></div>
-    <form class="filter-form" action="" method="get">
-        <div class="scrollable-container">
-            <div class="section">
-                <label class="section__title" for="filter-by">Sort By</label>
-                <select id="filter-by" class="section__dropdown">
-                    <option value="date">Date</option>
-                    <option value="score">Score</option>
-                </select>
-                <segmented-control name="date">
-                    <button name="date" type="button" value="order:created">Newest</button>
-                    <button name="date" type="button" value="order:created_asc">Oldest</button>
-                </segmented-control>
-                <segmented-control name="score" disabled>
-                    <button name="score" type="button" value="order:score">Most Popular</button>
-                    <button name="score" type="button" value="order:score_asc">Least Popular</button>
-                </segmented-control>
-            </div>
-            <div class="section">
-                <div class="section__title">Ratings</div>
-                <div class="section__buttons">
-                    <input type="checkbox" name="rating-safe" class="section__button section__button--safe" value="rating:safe" text="Safe">
-                    <input type="checkbox" name="rating-questionable" class="section__button section__button--questionable" value="rating:questionable" text="Questionable" disabled>
-                    <input type="checkbox" name="rating-explicit" class="section__button section__button--explicit" value="rating:explicit" text="Explicit" disabled>
-                </div>
-            </div>
-            <div class="section">
-                <div class="section__title">Apply</div>
-                <div class="section__buttons">
-                    <input type="checkbox" name="extra-blacklist" class="section__button" value="blacklist" text="Blacklist" disabled>
-                    <input type="checkbox" name="extra-favorites" class="section__button" value="fav" text="Favorites" disabled>
-                </div>
-            </div>
-            <div class="confirmation">
-                <button type="button" name="cancel" class="confirmation__button" disabled>Cancel</button>
-                <button type="button" name="apply" class="confirmation__button confirmation__button--apply">Apply</button>
-            </div>
+    <div class="header">
+        <button class="header__exit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+                <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+                <path d="m9.88 12.71-4.95 4.95 1.41 1.41 2.83-2.83L12 13.41l2.12 2.13 3.54 3.53 1.41-1.41-4.95-4.95-.71-.71 5.66-5.66-1.41-1.41L12 10.59 6.34 4.93 4.93 6.34 10.59 12z"></path>
+            </svg>
+        </button>
+        <div class="header__id">
+            <span class="header__id-logo" style="font-weight: bolder;">ID</span>
+            <span class="header__id-num">99999</span>
         </div>
-    </form>
+        <button class="header__info-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" >
+                <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+                <path d="M12 22c5.51 0 10-4.49 10-10S17.51 2 12 2 2 6.49 2 12s4.49 10 10 10M11 7h2v2h-2zm0 4h2v6h-2z"></path>
+            </svg>
+        </button>
+    </div>
+    <image-container class="image-container">
+        <score-fav class="score-fav" score="00k+" slot="score-fav"></score-fav>
+    </image-container>
+    <sub-menu></sub-menu>
 
     <style>${style}</style>
 `;
 
-export class FilterMenu extends HTMLElement {
+export class FullView extends HTMLElement {
     #root;
-    #elems = {};
-
-    #internals;
-    static formAssociated = true;
+    #elems = {}; // header, imageContainer, scoreFav, subMenu
+    // #state = {}; // postsData, selectedPost
+    #postsData = {};
 
     constructor() {
         super();
 
-        this.#root = this.attachShadow({ mode: 'closed', delegatesFocus: true });
+        this.#root = this.attachShadow({ mode: 'closed' });
         this.#root.append(template.content.cloneNode(true));
-        this.#internals = this.attachInternals();
     }
 
     static get observedAttributes() {
-        return [];
+        return ['open', 'fullscreen']
     }
 
     connectedCallback() {
-        this.#elems.sections = this.#root.querySelectorAll('.section');
-        this.#elems.dropdown = this.#root.querySelector('.section__dropdown');
-        this.#elems.form = this.#root.querySelector('.filter-form');
-
-        this.#saveOptions();
-
-        this.#root.querySelector('.confirmation__button[name="apply"]').addEventListener('click', this.#saveOptions.bind(this));
-        this.#root.querySelector('.confirmation__button[name="apply"]').addEventListener('click', this.#closeFilterMenu.bind(this));
-        this.#root.querySelector('.confirmation__button[name="cancel"]').addEventListener('click', this.#closeFilterMenu.bind(this));
-        this.#elems.dropdown.addEventListener('change', this.#handleDropdownChange.bind(this));
+        this.#elems.header = this.#root.querySelector('.header');
+	    this.#elems.imageContainer = this.#root.querySelector('image-container');
+	    this.#elems.scoreFav = this.#root.querySelector('score-fav');
+	    this.#elems.submenu = this.#root.querySelector('sub-menu');
+        
+        this.addEventListener('selectPost', (e) => this.#setUiElems(e.detail));
+        this.addEventListener('fullscreen', () => this.toggleAttribute('fullscreen'));
+        // this.addEventListener('submenuMove', this.#handleSubmemuHeight);
+        // this.addEventListener('submenuDrop', this.#handleSubmenuDrop);
+	    this.#elems.header.querySelector('.header__exit').addEventListener('click', this.#closeFullView.bind(this));
     }
 
     disconnectedCallback() {
-        this.#root.querySelector('.confirmation__button[name="apply"]').removeEventListener('click', this.#saveOptions.bind(this));
-        this.#root.querySelector('.confirmation__button[name="apply"]').removeEventListener('click', this.#closeFilterMenu.bind(this));
-        this.#root.querySelector('.confirmation__button[name="cancel"]').removeEventListener('click', this.#closeFilterMenu.bind(this));
-        this.#elems.dropdown.removeEventListener('change', this.#handleDropdownChange.bind(this));
+        this.addEventListener('selectPost', (e) => this.#setUiElems(e.detail));
+        this.removeEventListener('fullscreen', this.#fullscreen.bind(this));
+        // this.removeEventListener('submenuMove', this.#handleSubmemuHeight);
+        // this.removeEventListener('submenuDrop', this.#handleSubmenuDrop);
+	    this.#elems.header.querySelector('.header__exit').removeEventListener('click', this.#closeFullView.bind(this));
     }
 
-    attributeChangedCallback(name, oldValue, newValue) {}
-
-    #saveOptions() {
-        const formData = new FormData(this.#elems.form);
-        this.#internals.setFormValue(formData);
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'fullscreen') this.#fullscreen();
     }
 
-    #handleDropdownChange(event) {
-        const dropdownValue = event.target.value;
-        
-        this.#root.querySelector('segmented-control:not([disabled])').toggleAttribute('disabled');
-        this.#root.querySelector(`segmented-control[name="${dropdownValue}"]`).removeAttribute('disabled');
+    set postsData(data) {
+        this.#postsData = data;
+    };
+
+    openFullView(postIndex) {
+        this.setAttribute('open', '');
+
+        this.#elems.imageContainer.setSlider(this.#postsData);
+        this.#elems.imageContainer.scrollToX(postIndex);
+
+        this.#setUiElems(postIndex);
     }
 
-    #closeFilterMenu() {
+    #closeFullView() {
         this.removeAttribute('open');
+        this.removeAttribute('fullscreen');
+        this.#elems.imageContainer.scrollToX(0);
+        this.#elems.submenu.tempReset();
+    };
+
+    #setUiElems(postIndex) {
+        if (postIndex == null) return;
+        const post = this.#postsData[postIndex];
+        
+        this.#elems.header.querySelector('.header__id').innerHTML = post.id
+        this.#elems.scoreFav.setAttribute('score', post.score.total);
+        this.#elems.submenu.setAttribute('url', post.file.url);
+        this.#elems.submenu.setAttribute('id', post.id);
+        this.#elems.submenu.setAttribute('ext', post.file.ext);
     }
+
+    #fullscreen() {
+        this.#elems.submenu.classList.remove('submenu--open');
+        this.#elems.submenu.style.height = '0%';
+
+        const shittySafariForceRepaint = () => {
+            this.style.display = 'none';
+            this.offsetHeight;
+            this.style = null;
+        }
+
+        // const chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36';
+        // const arc    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36';
+        // const edge   = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0';
+        // const opera  = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0s';
+        // const safari = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1';
+
+        const isSafari = /iPhone/i.test(navigator.userAgent);
+        if (isSafari)
+            requestAnimationFrame(shittySafariForceRepaint);
+
+        const fullScreenEvent = new CustomEvent('fullScreen', {
+            bubbles: true,
+            composed: true
+        });
+        this.dispatchEvent(fullScreenEvent);
+    }
+
+    #handleSubmemuHeight(fingerPostCalculationInEventDetail) {
+        this.#elems.submenu.setHeight(fingerPostCalculationInEventDetail.detail);
+    };
+    #handleSubmenuDrop() {
+        this.#elems.submenu.setHeight();
+    };
+
 }
-defineCustomElement('filter-menu', FilterMenu);
+defineCustomElement('full-view', FullView);
